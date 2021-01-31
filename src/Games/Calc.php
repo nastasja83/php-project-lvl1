bcf80b285e2acaafad799b0f547d95338e6fa53c
Anastasiya Ostertag <nastasja83@mail.ru>
2021-01-31 14:25:22 +0700

    create function  getVariant

--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -1,12 +1,13 @@
 <?php
 
-namespace Brain\Games\Games\Calc;
+namespace BrainGames\Games\Calc;
 
 use function Brain\Games\Engine\flow;
 
 const MIN_INTEGER = 2;
 const MAX_INTEGER = 30;
 const OPERATORS = ['+', '-', '*'];
+const RULES = "What is the result of the expression?";
 
 function getIntegers()
 {
@@ -18,30 +19,32 @@ function getOperator()
     return array_rand(array_flip(OPERATORS));
 }
 
-function runCalc()
+function getVariant($int1, $int2, $operator)
 {
-    $rules = "What is the result of the expression?";
+    switch ($operator) {
+        case '+':
+            $question = "{$int1} + {$int2}";
+            $correctAnswer = $int1 + $int2;
+            break;
+        case '-':
+            $question = "{$int1} - {$int2}";
+            $correctAnswer = $int1 - $int2;
+            break;
+        case '*':
+            $question = "{$int1} * {$int2}";
+            $correctAnswer = $int1 * $int2;
+            break;
+    }
+    return [$question, (string) $correctAnswer];
+}
 
+function run()
+{
     $gameData = function (): array {
         $int1 = getIntegers();
         $int2 = getIntegers();
         $operator = getOperator();
-
-        switch ($operator) {
-            case '+':
-                $question = "{$int1} + {$int2}";
-                $correctAnswer = $int1 + $int2;
-                break;
-            case '-':
-                $question = "{$int1} - {$int2}";
-                $correctAnswer = $int1 - $int2;
-                break;
-            case '*':
-                $question = "{$int1} * {$int2}";
-                $correctAnswer = $int1 * $int2;
-                break;
-        }
-        return [$question, (string) $correctAnswer];
+        return getVariant($int1, $int2, $operator);
     };
-    flow($gameData, $rules);
+    flow($gameData, RULES);
 }
